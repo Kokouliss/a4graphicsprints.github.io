bc844bf4f7c463195b15fcb67c73cab7c9a9337c
Kokoulis <kokouliss@gmail.com>
2019-05-10 22:50:21 +0300

    my first one

---
+++ jss/offcanvas.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){console.log("document is ready");$(".navbar-nav").clone().prependTo("#off-canvas");$(function(){$(document).trigger("enhance")})});$(window).load(function(){console.log("window is loaded")})
